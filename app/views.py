0ce4b5be87c51e290269c10d42d3324df3ee6a81
omar nazih <omarnazeh295@gmail.com>
2022-11-16 01:38:10 +0200

    fix - no action in form

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from os import path
 from app import app
 
 
-FILE_PATH = app.config['DOWNLOADS_FOLDER']    
+FILE_PATH = app.config['DOWNLOADS_FOLDER']
 
 
 @app.route('/')
@@ -18,25 +18,25 @@ def index():
 
 @app.route('/get-video', methods=['GET', 'POST'])
 def get_video():
-    url = request.form.get('url')    
+    url = request.form.get('url')
     yt = YouTube(url)
-    
+
     file_name: str = secure_filename(f'{yt.title}.mp4')
 
-    yt.streams.filter(file_extension='mp4')    
-    stream = yt.streams.get_by_itag(22)      
-    
+    yt.streams.filter(file_extension='mp4')
+    stream = yt.streams.get_by_itag(22)
+
     stream.download(FILE_PATH, filename=file_name)
 
     try:
         flash("Downloading")
         return send_from_directory(FILE_PATH, file_name, as_attachment=True)
-    except FileNotFoundError:        
+    except FileNotFoundError:
         abort(404)
 
 
 @app.errorhandler(400)
 @app.errorhandler(404)
 @app.errorhandler(405)
-def _handle_api_error(ex):        
+def _handle_api_error(ex):
     return render_template('error.html', msg=ex, code=ex.code)
